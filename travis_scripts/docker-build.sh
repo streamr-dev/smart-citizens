5763e032653968301833165a57daca7f90e3e820
mikhaelsantos <mikhaelsantos@gmail.com>
2018-05-07 15:19:20 +0300

    Add travis and Docker conf

--- travis_scripts/docker-build.sh
+++ travis_scripts/docker-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export OWNER=<owner>
-export IMAGE_NAME=<image_name>
+export OWNER=streamr
+export IMAGE_NAME=smart-citizen
 export VCS_REF=`git rev-parse --short HEAD`
 export IMAGE_VERSION=0.2.${TRAVIS_BUILD_NUMBER}
 export QNAME=${OWNER}/${IMAGE_NAME}
